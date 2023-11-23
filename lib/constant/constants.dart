0d66bd46c658cbd9e024c5fe810eec450819a1e1
Azeemjalageri <83650235+Azeemjalageri@users.noreply.github.com>
2023-11-23 15:01:33 +0530

    Add files via upload

---
+++ lib/constant/constants.dart
@@ -0,0 +1,13 @@
+String mapBoxApiKey =
+    'pk.eyJ1IjoiMDFmZTIwYmNzMTkxIiwiYSI6ImNsaDh0ODlkbDAwdTMzcXFmeWd6czhtMnIifQ.A1vY_76AeQq7nKCjc7oOgA';
+
+String nftStorageApiKey =
+    // '5ReEuIxsDhWxOLa2lVe9nB2PUjdEkwJ5jLsBGdBDGA.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.JuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOi';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDlENzk3ZTVFQzhGQ2Y4RDQzYTY4RTYzM2Y0MzU3YzFFN2ZkMTMzN0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4MzI3MTAzNjYwMCwibmFtZSI6IkxBTkQgUkVHSVNUUlkifQ.sqG1R58OJi0XsIwSO94V8hSI09cbYhmF32SXs4dfOHs';
+const String rpcUrl =
+    // "https://rpc-mumbai.maticvigil.com/v1/a5be973518c173bacd9be16a6314dd08b6abcd23";
+    "http://127.0.0.1:8545";
+
+const String contractAddress = "0x4D00B1843442E3cc68c0731e01A4C7FCa1787bdE";
+
+const int chainId = 4321; //Polygon testnet-80001, local ganache-1337
